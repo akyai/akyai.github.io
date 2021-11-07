3bb56581ba5b5a643f3b71fd1b7ba1d2445a02d2
akyai <75884355+akyai@users.noreply.github.com>
2021-11-07 19:17:13 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const request = new Request('https://akyai.github.io',{method: 'POST', body: '{"foo": "bar"}'});
-
-const URL = request.url;
-const method = request.method;
-const credentials = request.credentials;
-
-fetch(request)
-  .then(response => console.log(response))
